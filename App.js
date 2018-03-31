3606de5db882059c29a961ea7cd3cb3237991617
vicaguilarsanz <vicaguilarsanz@gmail.com>
2018-03-31 20:00:14 +0000

    Auth Integrado

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { StyleSheet, View, Image, Text, KeyboardAvoidingView, ImageBackground } from 'react-native';
-
+import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
 import Router from './components/Router';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyDpcBS3qQruhxurrNM3uJGNeOg5j1mghUw",
+      authDomain: "ai-billing-vg.firebaseapp.com",
+      databaseURL: "https://ai-billing-vg.firebaseio.com",
+      projectId: "ai-billing-vg",
+      storageBucket: "ai-billing-vg.appspot.com",
+      messagingSenderId: "523439758450"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Router />
--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import { StyleSheet, View, TextInput, Text, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-
+import firebase from 'firebase';
 export default class App extends React.Component {
+
+  state = { email: '', password: '', error: '', loading: false };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+    this.setState({ error: '', loading: true });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
+      });
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      error: '',
+      loading: false
+    });
+    Actions.menu();
+  }
+
+  onLoginFail() {
+    this.setState({ error: 'Authentication Failed.', loading: false });
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -12,6 +42,8 @@ export default class App extends React.Component {
           onSubmitEditing={() => this.passwordInput.focus()}
           keyboardType="email-address"
           style={styles.input}
+          value={this.state.email}
+          onChangeText={email => this.setState({ email })} 
         />
 
         <Text style={styles.title}>Password</Text>
@@ -20,17 +52,23 @@ export default class App extends React.Component {
           secureTextEntry
           style={styles.input}
           ref={(input) => this.passwordInput = input}
+          value={this.state.password}
+          onChangeText={password => this.setState({ password })}
         />
 
         <TouchableOpacity 
           style={styles.buttonContainer}
-          onPress={() => Actions.menu()}
+          // onPress={() => Actions.menu()}
+          onPress={this.onButtonPress.bind(this)}
         >
           <Text style={styles.buttonText}>LOGIN</Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.RContainer}><Text>Sign Up</Text></TouchableOpacity>
 
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
 
       </View>
     );
@@ -71,7 +109,10 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     paddingVertical: 5,
     alignItems: 'center'
-
+  },
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
-
 });
